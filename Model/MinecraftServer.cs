9eb90d73521348bdeac66ab0a7d4018710f7291d
Semih Teke <semih.teke@student.howest.be>
2023-06-06 12:18:18 +0200

    updated the minecraft server model Added the main server list page ui

--- Model/MinecraftServer.cs
+++ Model/MinecraftServer.cs
@@ -8,5 +8,27 @@ namespace MinecraftServerStatusChecker.Model
 {
     public class MinecraftServer
     {
+        public bool IsOnline { get; set; }
+        public string HostName { get; set; }
+        public string IpAddress { get; set; }
+        public int ServerPort { get; set; }
+        public string GameVersion { get; set; }
+        public string MessageOfTheDay { get; set; }
+        public Players Players { get; set; }
+        public string Base64Icon { get; set; } //base64 encoded, decode to display
+        public string MapName { get; set; }
+        public string GameMode { get; set; }
+        public string[] Plugins { get; set; }
+        public string[] Mods { get; set; }
+
+    }
+
+    public class Players
+    {
+        public int Online { get; set; }
+        public int MaxPlayers { get; set; }
+        public string[] PlayerList { get; set; } //might be empty if server settings don't allow for player querying
     }
 }
+
+
